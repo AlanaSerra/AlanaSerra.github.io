fb0a5617f503ba7ab09e2dc14eef13a969bb6979
AlanaSerra <alana.scherer@estudante.ifms.edu.br>
2025-01-30 10:08:17 -0400

    nao foi

--- src/routes/academica/+page.svelte
+++ src/routes/academica/+page.svelte
@@ -6,11 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
     <style>
-        body {
-            font-family: 'Poppins', sans-serif;
-            background: linear-gradient(to bottom, #680a1d, #680a1d); 
-            color: #f8f9fa;
-        }
+        
         header {
             background-color: #003f69; 
             color: #fff;
